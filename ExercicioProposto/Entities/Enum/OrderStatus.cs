5f4f74584a9f59af8382f87e22d7868d1d6dd823
Paulo3678 <ph7811001@fmail.com>
2023-03-12 10:03:30 -0300

    :sparkles: First commit

---
+++ ExercicioProposto/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioProposto.Entities.Enum
+{
+    enum OrderStatus
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ ExercicioProposto/Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using ExercicioProposto.Entities;
+using ExercicioProposto.Entities.Enum;
+
+namespace ExercicioProposto.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; private set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+            Moment = DateTime.Now;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach (OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+
+            return total;
+        }
+
+    }
+}
